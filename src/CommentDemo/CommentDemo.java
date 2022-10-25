794daa68898eba2d7cd1e4384d69b71aff7d4c38
unknown <engr.mohammadjannatulnayeem@gmail.com>
2022-10-26 02:20:32 +0600

    code uploaded

--- src/CommentDemo/CommentDemo.java
+++ src/CommentDemo/CommentDemo.java
@@ -1,14 +1,33 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package CommentDemo;
 
-/**
- *
- * @author nayeem
- */
+
 public class CommentDemo {
+    public static void main(String[]args)
+    {
+    
+        int x=10;//Single line comment
+        
+        
+     /*int y=2;     
+     
+     float a=23.5f;
+       Multi-line comment
+     */
+     
+     
+     /**
+      * Documentation Comment
+     System.out.println("bangladesh");
+     for(int i=0;i<10;i++)
+     {
+     
+     
+     }
+   
+  */
+    
+    
+    }
     
 }
